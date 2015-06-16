962ba3c0a479219d58c999add10973f43b77a911
evil0ps <909070781@qq.com>
2015-06-16 14:30:57 +0800

    java utils

--- Junit/evilp0s/ChinesUtilTest.java
+++ Junit/evilp0s/ChinesUtilTest.java
@@ -17,4 +17,26 @@ public class ChinesUtilTest extends TestCase {
         }
     }
 
+    @Test
+    public void testChineseLength() {
+        String input = "234判234断一的fg456个字符rer串d23213fg中有de多少g45fhh个中文324";
+        System.out.println(ChinesUtil.ChineseLength(input));
+    }
+
+    @Test
+    public void testIsChinese() {
+        String input0 = "234判234断一的fg456个字符rer串d23213fg中有de多少g45fhh个中文324";
+        String input1 = "Process finished with exit code 0";
+        System.out.println(ChinesUtil.isChinese(input0));
+        System.out.println(ChinesUtil.isChinese(input1));
+    }
+
+    @Test
+    public void testgetStringLen() {
+        String str1 = "234判断fg456";
+        System.out.println(StringUtil.getStringLen(str1));
+        System.out.println(ChinesUtil.ChineseLength(str1));
+    }
+
+
 }
--- src/evilp0s/ChinesUtil.java
+++ src/evilp0s/ChinesUtil.java
@@ -8,6 +8,7 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -94,29 +95,9 @@ public class ChinesUtil {
         return pybf.toString();
     }
 
-    // 根据Unicode编码完美的判断中文汉字和符号
-    private static boolean isChinese(char c) {
-        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
-        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
-                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
-                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
-                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION) {
-            return true;
-        }
-        return false;
-    }
 
-    // 完整的判断中文汉字和符号
-    public static boolean isChinese(String strName) {
-        char[] ch = strName.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-            char c = ch[i];
-            if (isChinese(c)) {
-                return true;
-            }
-        }
-        return false;
-    }
+
+
 
     // 只能判断部分CJK字符（CJK统一汉字）
     public static boolean isChineseByREG(String str) {
@@ -139,4 +120,79 @@ public class ChinesUtil {
         return pattern.matcher(str.trim()).find();
     }
 
+
+    // 完整的判断中文汉字和符号
+    public static boolean isChinese(String strName) {
+        char[] ch = strName.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            char c = ch[i];
+            if (isChinese(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * 判断是否是中文
+     *
+     * @param c
+     * @return
+     */
+    public static boolean isChinese(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取一个字符串中中文字符的个数
+     */
+    public static int ChineseLength(String str) {
+        Pattern p = Pattern.compile("[\u4E00-\u9FA5]+");
+        Matcher m = p.matcher(str);
+        int i = 0;
+        while (m.find()) {
+            String temp = m.group(0);
+            i += temp.length();
+        }
+        return i;
+    }
+
+    /**
+     * 判断是否是乱码
+     *
+     * @param strName
+     * @return
+     */
+    public static boolean isMessyCode(String strName) {
+        Pattern p = Pattern.compile("\\s*|\t*|\r*|\n*");
+        Matcher m = p.matcher(strName);
+        String after = m.replaceAll("");
+        String temp = after.replaceAll("\\p{P}", "");
+        char[] ch = temp.trim().toCharArray();
+        float chLength = 0;
+        float count = 0;
+        for (int i = 0; i < ch.length; i++) {
+            char c = ch[i];
+            if (!Character.isLetterOrDigit(c)) {
+                if (!ChinesUtil.isChinese(c)) {
+                    count = count + 1;
+                }
+                chLength++;
+            }
+        }
+        float result = count / chLength;
+        if (result > 0.4) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
