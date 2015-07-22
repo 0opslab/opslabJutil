1cd91f14a3b3e45a928aab530b025e58b101ae2f
evil0ps <909070781@qq.com>
2015-07-22 19:30:49 +0800

    linux test

--- Junit/evilp0s/EmailUtilTest.java
+++ Junit/evilp0s/EmailUtilTest.java
@@ -9,8 +9,9 @@ public class EmailUtilTest  {
 
     public void testMail() {
         EmailUtil se = new EmailUtil(false);
+        String path =System.getProperty("user.dir")+"/Junit/Resource/ali.gif";
         se.doSendHtmlEmail("邮件主题", "邮件内容", "438558488@qq.com");
-        File affix = new File("c:/windows/system.ini");
+        File affix = new File(path);
         se.doSendHtmlEmail("邮件主题", "邮件内容", "438558488@qq.com", affix);//
     }
 }
