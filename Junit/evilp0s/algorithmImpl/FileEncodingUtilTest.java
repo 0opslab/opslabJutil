f4709fc3c30a65e421ec535bc99da2fe23934329
evil0ps <909070781@qq.com>
2015-07-25 20:08:18 +0800

    java utils

--- Junit/evilp0s/algorithmImpl/FileEncodingUtilTest.java
+++ Junit/evilp0s/algorithmImpl/FileEncodingUtilTest.java
@@ -8,17 +8,12 @@ import java.io.FilenameFilter;
 
 public class FileEncodingUtilTest extends TestCase {
 
-    public void testConvert() throws Exception {
+    public void testConvert()   {
         String file = SysUtil.CURRENT_USER_DIR + "/Junit/Resource/GBKTOUTF8.txt";
-        FileEncodingUtil.convert(file, "GBK", "UTF-8", new FilenameFilter() {
-                    @Override
-                    public boolean accept(File dir, String name) {
-                        return name.endsWith("txt");
-                    }
-                });
+        FileEncodingUtil.convert(file, "GBK", "UTF-8");
     }
 
-    public void testConvert1() throws Exception {
+    public void testConvert1() {
         String file = SysUtil.CURRENT_USER_DIR + "/Junit/Resource/GBKTOUTF8.txt";
         FileEncodingUtil.convert(file, "UTF-8", "GBK", new FilenameFilter() {
                     @Override
@@ -28,19 +23,8 @@ public class FileEncodingUtilTest extends TestCase {
                 });
     }
 
-    public void testConvert2() throws Exception {
-
-    }
-
-    public void testConvert3() throws Exception {
-
-    }
-
-    public void testGetFileContentFromCharset() throws Exception {
-
-    }
-
-    public void testSaveFile2Charset() throws Exception {
-
+    public void testConvert2(){
+        String file = SysUtil.CURRENT_USER_DIR + "/Junit/Resource/GBKTOUTF8.txt";
+        FileEncodingUtil.convert(new File(file),"GBK", "UTF-8");
     }
 }
