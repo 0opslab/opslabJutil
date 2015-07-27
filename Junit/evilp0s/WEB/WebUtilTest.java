7b43badad496f31142ac43bc910683a89ac939c6
evil0ps <909070781@qq.com>
2015-07-27 10:17:43 +0800

    java utils

--- Junit/evilp0s/WEB/WebUtilTest.java
+++ Junit/evilp0s/WEB/WebUtilTest.java
@@ -1,16 +1,17 @@
 package evilp0s.WEB;
 
+import evilp0s.PrintUtil;
 import junit.framework.TestCase;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class WebUtilTest extends TestCase {
 
 
     @Test
-    public void test() {
-        StringBuffer html   = new StringBuffer();
-        StringBuffer result = new StringBuffer();
-
+    public void testescape() {
+        StringBuffer html = new StringBuffer();
         html.append("<div class=\"ipaddress\">服务器IP地址：</div>");
         html.append("<div class=\"ipinput\">");
         html.append("<input type=\"text\" value=\"127.0.5.5.0\" class=\"text\" />");
@@ -22,14 +23,16 @@ public class WebUtilTest extends TestCase {
         html.append("<span class=\"content\">②修改服务器IP，会导致所有的GameServer重启，以便重新加载配置</span>");
         html.append("</div>");
         String unescape = html.toString();
-        String escape   = WebUtil.escape(unescape);
+        String escape = WebUtil.escape(unescape);
         System.out.println(escape);
         unescape = WebUtil.unescape(escape);
         System.out.println(unescape);
         System.out.println(unescape.equals(html.toString()));
-
         System.out.println(WebUtil.escape("123 123"));
+    }
 
+    @Test
+    public void testUrl(){
         String url = "http://www.baidu.com/page.jsp?act=list&ad=12&redirect=true";
 
         //设置参数
@@ -42,5 +45,29 @@ public class WebUtilTest extends TestCase {
         System.out.println(WebUtil.removeParam(url, "act", "ad", "redirect"));
 
     }
+
+    @Test
+    public void testParseQuery(){
+        String query ="id=111&name=test&password=p0ssw0rd";
+        Map<String,String> queryMap = WebUtil.parseQuery(query, '&', '=', null);
+        PrintUtil.print(queryMap);
+        Map<String,String> httpQueryMap = WebUtil.httpParseQuery(query);
+        PrintUtil.print(httpQueryMap);
+
+        //数组解析
+        String query2 ="id=111&name=test&password[]=p0ssw0rd&password[]=123456";
+        Map<String,String> queryMap1 = WebUtil.parseQuery(query2, '&', '=', ",");
+        PrintUtil.print(queryMap1);
+        Map<String,String> httpQueryMap1 = WebUtil.httpParseQuery(query2);
+        PrintUtil.print(httpQueryMap1);
+
+        //"id=111&name=test&password=p0ssw0rd,=123456"
+        String query3 ="id=111&name=test&password=p0ssw0rd,123456";
+        Map<String,String> queryMap2 = WebUtil.parseQuery(query3, '&', '=', ",");
+        PrintUtil.print(queryMap2);
+        Map<String,String> httpQueryMap2 = WebUtil.httpParseQuery(query3);
+        PrintUtil.print(httpQueryMap2);
+    }
+
 
 }
