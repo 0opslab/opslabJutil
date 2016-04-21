9ea06c3a934f79c233140cdbd39e984af73e89ff
0opslab <909070781@qq.com>
2016-04-21 15:43:27 +0800

    valid
    valid test

--- junit/com/opslab/validTest.java
+++ junit/com/opslab/validTest.java
@@ -8,26 +8,26 @@ import java.util.Map;
 public class validTest extends TestCase {
 
     public void testIsValid() throws Exception {
-        assertEquals(false, valid.isValid(""));
-        assertEquals(true, valid.isValid("111"));
-        assertEquals(true, valid.isValid("111", "2222"));
-        assertEquals(false, valid.isValid("111", ""));
+        assertEquals(false, valid.valid(""));
+        assertEquals(true, valid.valid("111"));
+        assertEquals(true, valid.valid("111", "2222"));
+        assertEquals(false, valid.valid("111", ""));
     }
 
     public void testIsValid1() throws Exception {
-        assertEquals(true, valid.isValid(new String("11")));
+        assertEquals(true, valid.valid(new String("11")));
 
     }
 
     public void testIsValid2() throws Exception {
         Map map = new HashMap();
-        assertEquals(false, valid.isValid(map));
+        assertEquals(false, valid.valid(map));
         map.put("1", "1");
-        assertEquals(true, valid.isValid(map));
+        assertEquals(true, valid.valid(map));
         Map map1 = new HashMap();
-        assertEquals(false, valid.isValid(map, map1));
+        assertEquals(false, valid.valid(map, map1));
         map1.put("2", "2");
-        assertEquals(true, valid.isValid(map, map1));
+        assertEquals(true, valid.valid(map, map1));
     }
 
     public void testIsDate(){
