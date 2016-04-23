dca2ee81e34078248f76f5ba264adb74b50e7aec
0ps-Human <909070781@qq.com>
2016-04-23 17:59:35 +0800

    delete
    delte temp image file

--- junit/com/opslab/image/OperateImageTest.java
+++ junit/com/opslab/image/OperateImageTest.java
@@ -26,11 +26,11 @@ public class OperateImageTest extends TestCase {
         String readImageFormat = "jpg";
         String writeImageFormat = "jpg";
         //从左上角剪切图片
-        OperateImage.cropImage(file, path + "cropImage100x100-0.jpg", 0, 0, 100, 100,
+        OperateImage.cropImage(file, path_temp + "cropImage100x100-0.jpg", 0, 0, 100, 100,
                 readImageFormat,
                 writeImageFormat);
         //从中间剪切图片
-        OperateImage.cropImage(file, path + "cropImage100x100-1.jpg", 100, 100, 100, 100,
+        OperateImage.cropImage(file, path_temp + "cropImage100x100-1.jpg", 100, 100, 100, 100,
                 readImageFormat,
                 writeImageFormat);
     }
@@ -56,9 +56,9 @@ public class OperateImageTest extends TestCase {
         BufferedImage img = ImageIO.read(new File(file));
         System.out.println(img.getWidth()+":"+img.getHeight());
         //长高等比缩小
-        OperateImage.reduceImageEqualProportion(file,path+"ReduceImageEqualx1.jpg",1);
-        OperateImage.reduceImageEqualProportion(file, path + "ReduceImageEqualx2.jpg", 2);
-        OperateImage.reduceImageEqualProportion(file, path + "ReduceImageEqualx5.jpg", 5);
+        OperateImage.reduceImageEqualProportion(file,path_temp+"ReduceImageEqualx1.jpg",1);
+        OperateImage.reduceImageEqualProportion(file, path_temp + "ReduceImageEqualx2.jpg", 2);
+        OperateImage.reduceImageEqualProportion(file, path_temp + "ReduceImageEqualx5.jpg", 5);
     }
 
     public void testEnlargementImageByRatio() throws Exception {
@@ -66,8 +66,8 @@ public class OperateImageTest extends TestCase {
         BufferedImage img = ImageIO.read(new File(file));
         System.out.println(img.getWidth()+":"+img.getHeight());
         //长高放大
-        OperateImage.enlargementImageByRatio(file, path + "enlargementImageByRatio1x1.jpg", 1, 1);
-        OperateImage.enlargementImageByRatio(file, path + "enlargementImageByRatio2x2.jpg", 2, 2);
+        OperateImage.enlargementImageByRatio(file, path_temp + "enlargementImageByRatio1x1.jpg", 1, 1);
+        OperateImage.enlargementImageByRatio(file, path_temp + "enlargementImageByRatio2x2.jpg", 2, 2);
     }
 
     public void testEnlargementImageEqualProportion() throws Exception {
@@ -75,8 +75,8 @@ public class OperateImageTest extends TestCase {
         BufferedImage img = ImageIO.read(new File(file));
         System.out.println(img.getWidth()+":"+img.getHeight());
         //长高等比放大
-        OperateImage.enlargementImageEqualProportion(file, path + "enlargementImageEqualProportion2x2.jpg", 2);
-        OperateImage.enlargementImageEqualProportion(file, path + "enlargementImageEqualProportion5x5.jpg", 5);
+        OperateImage.enlargementImageEqualProportion(file, path_temp + "enlargementImageEqualProportion2x2.jpg", 2);
+        OperateImage.enlargementImageEqualProportion(file, path_temp + "enlargementImageEqualProportion5x5.jpg", 5);
     }
 
     public void testResizeImage() throws Exception {
@@ -84,17 +84,17 @@ public class OperateImageTest extends TestCase {
         BufferedImage img = ImageIO.read(new File(file));
         System.out.println(img.getWidth() + ":" + img.getHeight());
         //重置图形的边长大小
-        OperateImage.resizeImage(file,path+"resizeImage64x64.jpg",64,64);
-        OperateImage.resizeImage(file,path+"resizeImage32x32.jpg",32,32);
+        OperateImage.resizeImage(file,path_temp+"resizeImage64x64.jpg",64,64);
+        OperateImage.resizeImage(file,path_temp+"resizeImage32x32.jpg",32,32);
     }
 
     public void testJoinImagesHorizontal() throws Exception {
         String file1 = path+"1.jpg";
         String file2 = path+"2.jpg";
         String file3 = path+"4.jpg";
-        String dstFile = path +"JoinImagesHorizontal-01.jpg";
-        String dstFile1 = path + "JoinImagesHorizontal-02.jpg";
-        String dstFile2 = path + "JoinImagesHorizontal-03.jpg";
+        String dstFile = path_temp +"JoinImagesHorizontal-01.jpg";
+        String dstFile1 = path_temp + "JoinImagesHorizontal-02.jpg";
+        String dstFile2 = path_temp + "JoinImagesHorizontal-03.jpg";
         OperateImage.joinImagesHorizontal(file1,file2,"jpg",dstFile);
         //OperateImage.joinImagesHorizontal(file1,file3,"jpg",dstFile1);
         //OperateImage.joinImagesHorizontal(file3,file1,"jpg",dstFile2);
@@ -102,7 +102,7 @@ public class OperateImageTest extends TestCase {
 
     public void testJoinImageListHorizontal() throws Exception {
         String[] files = new String[]{path+"1.jpg",path+"2.jpg",path+"3.jpg"};
-        String dst = path+"JoinImageListHorizontalx1.jpg";
+        String dst = path_temp+"JoinImageListHorizontalx1.jpg";
         //拼接一组图片
         OperateImage.joinImageListHorizontal(files, "jpg", dst);
     }
@@ -110,14 +110,14 @@ public class OperateImageTest extends TestCase {
     public void testJoinImagesVertical() throws Exception {
         String file1 = path+"1.jpg";
         String file2 = path+"2.jpg";
-        String dstFile = path +"JoinImagesVertical-01.jpg";
+        String dstFile = path_temp +"JoinImagesVertical-01.jpg";
         //纵向拼接俩张图片
         OperateImage.joinImagesVertical(file1, file2, "jpg", dstFile);
     }
 
     public void testJoinImageListVertical() throws Exception {
         String[] files = new String[]{path+"1.jpg",path+"2.jpg",path+"3.jpg"};
-        String dst = path+"JoinImageListVerticalx1.jpg";
+        String dst = path_temp+"JoinImageListVerticalx1.jpg";
         //拼接一组图片
         OperateImage.joinImageListVertical(files, "jpg", dst);
     }
@@ -139,7 +139,7 @@ public class OperateImageTest extends TestCase {
                 new String[]{"100","100",path+"reduceImageByRatio2X2.jpg"},
                 new String[]{"0","100",path+"reduceImageByRatio5X5.jpg"}
             };
-        String dstFile = path +"MergeImageList-01.jpg";
+        String dstFile = path_temp +"MergeImageList-01.jpg";
         OperateImage.mergeImageList(file1, Arrays.asList(files),"jpg",dstFile);
     }
 
@@ -181,7 +181,7 @@ public class OperateImageTest extends TestCase {
 
     public void testGrayImage() throws Exception {
         String file = path +"1.jpg";
-        OperateImage.grayImage(file,path+"GrayImage.jpg","jpg");
+        OperateImage.grayImage(file,path_temp+"GrayImage.jpg","jpg");
     }
 
     public void testAlphaWords2Image() throws Exception {
@@ -193,7 +193,7 @@ public class OperateImageTest extends TestCase {
         String inputWords = "www";
         String imageFormat = "jpg";
         String file = path +"1.jpg";
-        String dstFile = path+"AlphaWords2Image.jpg";
+        String dstFile = path_temp+"AlphaWords2Image.jpg";
         OperateImage.alphaWords2Image(file,alpha,font,fontStyle,fontSize,color,inputWords,0,0,imageFormat,dstFile);
     }
 
