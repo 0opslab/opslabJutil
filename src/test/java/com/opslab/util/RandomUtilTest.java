2b73611ac8493f80464e152d135ff3da9237e73d
0opslab <909070781@qq.com>
2016-11-03 23:09:38 +0800

    RandomUtil
    RandomUtil

--- src/test/java/com/opslab/util/RandomUtilTest.java
+++ src/test/java/com/opslab/util/RandomUtilTest.java
@@ -1,11 +1,15 @@
 package com.opslab.util;
 
 import com.opslab.util.RandomUtil;
+import com.opslab.util.collection.CollectionUtil;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import static com.opslab.util.RandomUtil.integer;
 import static junit.framework.Assert.assertEquals;
 
 
@@ -14,22 +18,41 @@ public class RandomUtilTest {
     @Test
     public void testInteger(){
         for(int i=0;i<10;i++){
-            System.out.println(RandomUtil.integer(0,10));
+            System.out.println(integer(0,10));
         }
         for(int i=0;i<10;i++){
-            System.out.println(RandomUtil.integer(20,30));
+            System.out.println(integer(20,30));
         }
 
         for(int i=0;i<10;i++){
-            System.out.println(RandomUtil.integer(100,110));
+            System.out.println(integer(100,110));
         }
+
+        for (int i = 0; i < 100000; i++) {
+            System.out.println(integer(1,1000000000));
+        }
+    }
+
+    @Test
+    public void testInteger1(){
+        Map<Integer,Integer> map = new HashMap();
+        for (int i = 0; i < 1000; i++) {
+            int integer = RandomUtil.integer(1, 100);
+            if(map.containsKey(integer)){
+                map.put(integer,map.get(integer)+1);
+
+            }else{
+                map.put(integer,1);
+            }
+        }
+        System.out.println(CollectionUtil.join(map,"\n","="));
     }
 
     @Test
     public void testName() throws Exception {
 
-        System.out.println(RandomUtil.integer(30,10));
-        System.out.println(RandomUtil.integer(0,10));
+        System.out.println(integer(30,10));
+        System.out.println(integer(0,10));
         System.out.println(RandomUtil.number(10));
         System.out.println(RandomUtil.number(10));
         System.out.println(RandomUtil.String(10));
@@ -58,6 +81,43 @@ public class RandomUtilTest {
             assertEquals("出现重复主键",100000,set.size());
         }
 
+    }
+    @Test
+    public void testRandomItem(){
+        for (int j = 0; j < 10; j++) {
+            Map<Integer,Integer> map = new HashMap();
+            for (int i = 0; i < 1000000; i++) {
+                Integer integer = RandomUtil.randomItem(new Integer[]{10, 30, 50});
+                if(map.containsKey(integer)){
+                    map.put(integer,map.get(integer)+1);
+                }else{
+                    map.put(integer,1);
+                }
+            }
+            int count = map.get(10)+map.get(30)+map.get(50);
+            String str = "10/30/50 ="+map.get(10)+":"+map.get(30)+":"+map.get(50)
+                    +"("+count+")";
+            System.out.println(str);
+        }
+    }
+    @Test
+    public void testRandomItem1(){
+        for (int j = 0; j < 10; j++) {
+            Map<Integer,Integer> map = new HashMap();
+            for (int i = 0; i < 1000000; i++) {
+                double[] percentum = new double[]{0.6,0.3,0.1};
+                Integer integer = RandomUtil.randomItem(new Integer[]{10, 30, 50}, percentum);
+                if(map.containsKey(integer)){
+                    map.put(integer,map.get(integer)+1);
+                }else{
+                    map.put(integer,1);
+                }
+            }
+            int count = map.get(10)+map.get(30)+map.get(50);
+            String str = "10/30/50 ="+map.get(10)+":"+map.get(30)+":"+map.get(50)
+                    +"("+count+")";
+            System.out.println(str);
+        }
     }
 
 }
