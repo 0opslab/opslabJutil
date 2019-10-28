09aee701a2ea8cccd6e9ca481029a4dbbd580cb3
0opslab <909070781@qq.com>
2019-10-28 14:34:45 +0800

    util

--- src/main/java/com/opslab/helper/HttpHelper.java
+++ src/main/java/com/opslab/helper/HttpHelper.java
@@ -619,4 +619,36 @@ public final class HttpHelper {
         }
         return map;
     }
+
+    /**
+     * 获取请求参数
+     * @param request
+     * @return
+     */
+    public static Map<String, Object> paramMaps(HttpServletRequest request) {
+        Map<String, String[]> properties = request.getParameterMap();
+        Map<String, Object> returnMap = new HashMap<String, Object>();
+        Iterator<?> entries = properties.entrySet().iterator();
+        Map.Entry<?, ?> entry;
+        String name = "";
+        String value = "";
+        while (entries.hasNext()) {
+            entry = (Map.Entry<?, ?>) entries.next();
+            name = (String) entry.getKey();
+            Object valueObj = entry.getValue();
+            if (null == valueObj) {
+                value = "";
+            } else if (valueObj instanceof String[]) {
+                String[] values = (String[]) valueObj;
+                for (int i = 0; i < values.length; i++) {
+                    value = values[i] + ",";
+                }
+                value = value.substring(0, value.length() - 1);
+            } else {
+                value = valueObj.toString();
+            }
+            returnMap.put(name, value);
+        }
+        return returnMap;
+    }
 }
