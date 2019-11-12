243245c57b13d1f8936ee1d476e04379192046e8
0opslab <909070781@qq.com>
2019-11-12 20:20:06 +0800

    util

--- src/main/java/com/opslab/helper/HttpHelper.java
+++ src/main/java/com/opslab/helper/HttpHelper.java
@@ -528,4 +528,30 @@ public final class HttpHelper {
         }
         return returnMap;
     }
+
+    public static String URLEncoderString(String str) {
+        String result = "";
+        if (null == str) {
+            return "";
+        }
+        try {
+            result = java.net.URLEncoder.encode(str, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String URLDecoderString(String str) {
+        String result = "";
+        if (null == str) {
+            return "";
+        }
+        try {
+            result = java.net.URLDecoder.decode(str, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
