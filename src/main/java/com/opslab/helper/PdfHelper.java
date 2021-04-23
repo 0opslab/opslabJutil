3229d9c66bbeb13fe2168c834069034142bdae1b
0opslab <909070781@qq.com>
2021-04-23 08:45:36 +0800

    PdfHelper

---
+++ src/main/java/com/opslab/helper/PdfHelper.java
@@ -0,0 +1,98 @@
+package com.opslab.helper;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.*;
+
+/**
+ * 一些pdf的操作
+ */
+public class PdfHelper {
+    /**
+     * Description: 将base64编码内容转换为Pdf
+     *
+     * @param base64编码内容，文件的存储路径（含文件名）
+     */
+    public static void base64StringToPdf(String base64Content, String filePath) {
+        BASE64Decoder decoder = new BASE64Decoder();
+        BufferedInputStream bis = null;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            byte[] bytes = decoder.decodeBuffer(base64Content);//base64编码内容转换为字节数组
+            ByteArrayInputStream byteInputStream = new ByteArrayInputStream(bytes);
+            bis = new BufferedInputStream(byteInputStream);
+            File file = new File(filePath);
+            File path = file.getParentFile();
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+
+            byte[] buffer = new byte[1024];
+            int length = bis.read(buffer);
+            while (length != -1) {
+                bos.write(buffer, 0, length);
+                length = bis.read(buffer);
+            }
+            bos.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bis.close();
+                fos.close();
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Description: 将pdf文件转换为Base64编码
+     *
+     * @param 要转的的pdf文件
+     */
+    public static String PDFToBase64(File file) {
+        BASE64Encoder encoder = new BASE64Encoder();
+        FileInputStream fin = null;
+        BufferedInputStream bin = null;
+        ByteArrayOutputStream baos = null;
+        BufferedOutputStream bout = null;
+        try {
+            fin = new FileInputStream(file);
+            bin = new BufferedInputStream(fin);
+            baos = new ByteArrayOutputStream();
+            bout = new BufferedOutputStream(baos);
+            byte[] buffer = new byte[1024];
+            int len = bin.read(buffer);
+            while (len != -1) {
+                bout.write(buffer, 0, len);
+                len = bin.read(buffer);
+            }
+            //刷新此输出流并强制写出所有缓冲的输出字节
+            bout.flush();
+            byte[] bytes = baos.toByteArray();
+            return encoder.encodeBuffer(bytes).trim();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fin.close();
+                bin.close();
+                bout.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+}
